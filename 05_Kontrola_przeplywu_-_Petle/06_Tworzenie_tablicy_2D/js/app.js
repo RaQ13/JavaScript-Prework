14c828f3c789742af3656204f1ee1a180bc229ec
RaQ13 <lukasz46@op.pl>
2022-10-14 13:24:59 +0200

    for loop is used to create an array with values setted in columns and rows variables

--- 05_Kontrola_przeplywu_-_Petle/06_Tworzenie_tablicy_2D/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/06_Tworzenie_tablicy_2D/js/app.js
@@ -1,1 +1,25 @@
-;
+const numbers = [];
+const columns = 4;
+const rows = 5;
+
+// for (let i = 0; i < rows; i++) {
+//     numbers[i] = [];
+//     for (let j = 0; j < columns; j++) {
+//
+//         numbers[i].push(j + i * columns + 1);
+//     }
+// }
+// console.log(numbers);
+
+/** drugie rozwiazanie */
+
+let k = 1;
+for (let i = 0; i < rows; i++) {
+    numbers.push([]);
+    for (let j = 0; j < columns; j++) {
+        numbers[i].push(k);
+        k++;
+    }
+}
+
+console.log(numbers);
