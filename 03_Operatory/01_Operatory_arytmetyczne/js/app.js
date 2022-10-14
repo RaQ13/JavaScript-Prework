8b8fc4092333dd34e3955542d06558c067a46e38
RaQ13 <lukasz46@op.pl>
2022-10-14 12:59:03 +0200

    variables with arithmetic operations are printed.

---
+++ 03_Operatory/01_Operatory_arytmetyczne/js/app.js
@@ -0,0 +1,15 @@
+let johnAge = 25;
+let aliceAge = 30;
+let bobAge = 17;
+
+let ageSum = johnAge + aliceAge;
+let ageModulo = aliceAge % johnAge;
+let ageDivide = aliceAge / bobAge;
+
+console.log(ageSum);
+console.log(ageModulo);
+console.log(ageDivide);
+
+let ageOperations = ageSum * ageModulo;
+
+console.log(ageOperations);
