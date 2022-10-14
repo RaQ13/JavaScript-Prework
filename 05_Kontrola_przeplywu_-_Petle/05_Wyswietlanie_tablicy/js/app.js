ff2a279bb8648be9e54b1dd9363e70f700764ef8
RaQ13 <lukasz46@op.pl>
2022-10-14 13:21:42 +0200

    for loop is used to print every element of 2D array

---
+++ 05_Kontrola_przeplywu_-_Petle/05_Wyswietlanie_tablicy/js/app.js
@@ -0,0 +1,15 @@
+const numbers = [
+    [16, 32, 2048],
+    [64, 256, 1024],
+    [8, 2, 4],
+];
+
+/**
+ * Write your code below!
+ */
+
+for (let i = 0; i < numbers.length; i++){
+    for (let j = 0; j < numbers[i].length; j++) {
+        console.log(numbers[i][j]);
+    }
+}
