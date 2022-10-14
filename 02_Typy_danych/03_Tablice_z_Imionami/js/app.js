d867a0d3dbf6791c9284d50393be2242f52c223d
RaQ13 <lukasz46@op.pl>
2022-10-14 12:19:56 +0200

    second, 5th and array length from first array are printed in first step. Names are pushed to new empty array. First, third and array length from second array are printed in second step.

---
+++ 02_Typy_danych/03_Tablice_z_Imionami/js/app.js
@@ -0,0 +1,17 @@
+const users = ["John", "Marie", "Kate", "Paul", "Steven"];
+
+console.log(users[1]);
+console.log(users[4]);
+console.log(users.length);
+
+const guests = [];
+
+guests.push("Chauncey");
+guests.push("Ling");
+guests.push("Ona");
+guests.push("Nicole");
+guests.push("Michaele");
+
+console.log(guests[0]);
+console.log(guests[2]);
+console.log(guests.length);
