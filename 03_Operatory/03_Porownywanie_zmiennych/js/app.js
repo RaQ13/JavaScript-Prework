a4d09619a2016df5ef8b732a6e686ed7fd8374ac
RaQ13 <lukasz46@op.pl>
2022-10-14 13:03:45 +0200

    equalation of content and type of content between variables is printed

---
+++ 03_Operatory/03_Porownywanie_zmiennych/js/app.js
@@ -0,0 +1,8 @@
+const numberAsNumber = 12;
+const numberAsString = "12";
+
+const equalValueAndType = numberAsNumber === numberAsString;
+console.log(equalValueAndType);
+
+const equalValue = numberAsNumber == numberAsString;
+console.log(equalValue);
