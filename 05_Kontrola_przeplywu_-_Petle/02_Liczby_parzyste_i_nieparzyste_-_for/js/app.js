93bbe2372c8013a8c161fdd7d69eb60f5017f45b
RaQ13 <lukasz46@op.pl>
2022-10-14 13:13:36 +0200

    for loop with if condition is used to print odd/even type of number

---
+++ 05_Kontrola_przeplywu_-_Petle/02_Liczby_parzyste_i_nieparzyste_-_for/js/app.js
@@ -0,0 +1,13 @@
+const n = 13;
+
+/**
+ * Write your code below!
+ */
+
+for (let i = 0; i <= n; i++) {
+    if (i % 2 == 0) {
+        console.log(i + " - even");
+    } else {
+        console.log(i + " - odd");
+    }
+}
