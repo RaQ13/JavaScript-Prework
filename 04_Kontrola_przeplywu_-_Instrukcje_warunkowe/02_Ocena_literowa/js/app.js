21bd0923c3c6a853831c06911803b52ad27c152d
RaQ13 <lukasz46@op.pl>
2022-10-14 13:08:54 +0200

    switch instruction on grade variable is used. Cases of example outpu are set

---
+++ 04_Kontrola_przeplywu_-_Instrukcje_warunkowe/02_Ocena_literowa/js/app.js
@@ -0,0 +1,27 @@
+const grade = 4;
+
+switch (grade) {
+    case 6: {
+        console.log("A");
+        break;
+    }
+    case 5: {
+        console.log("B");
+        break;
+    }
+    case 4: {
+        console.log("C");
+        break;
+    }
+    case 3: {
+        console.log("D");
+        break;
+    }
+    case 2: {
+        console.log("E");
+        break;
+    }
+    case 1: {
+        console.log("F");
+    }
+}
