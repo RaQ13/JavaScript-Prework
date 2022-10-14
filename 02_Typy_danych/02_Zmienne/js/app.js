216cb92c09b997132cec75fd919919e24f3eec59
RaQ13 <lukasz46@op.pl>
2022-10-14 12:15:21 +0200

    variables and typeofs their contents are printed

---
+++ 02_Typy_danych/02_Zmienne/js/app.js
@@ -0,0 +1,24 @@
+//ta zmienna jest typu number i przechowuje wartość 4
+let numberValue = 4;
+console.log(numberValue);
+console.log(typeof numberValue);
+
+//ta zmienna jest typu string i przechowuje wartość tekst
+let stringValue = "tekst";
+console.log(stringValue);
+console.log(typeof stringValue);
+
+//ta zmienna jest typu mixed i przechowuje wartości 4 liczbową i tekst tesktową
+let mixedValue = numberValue + " " + stringValue;
+console.log(mixedValue);
+console.log(typeof mixedValue);
+
+//ta zmienna jest typu logic i przechowuje wartość true
+let logicValue = true;
+console.log(logicValue);
+console.log(typeof logicValue);
+
+//ta zmienna jest typu null i przechowuje wartość null
+let nullValue = null;
+console.log(nullValue);
+console.log(typeof nullValue);
