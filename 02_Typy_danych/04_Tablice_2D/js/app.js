b36e8a9f70a9b88fc22db0694261466c27b58235
RaQ13 <lukasz46@op.pl>
2022-10-14 12:27:41 +0200

    array with 3 rows and 4 columns is printed. Second element from first row, second row and length of third row are printed in step 1. Array with mixed values is printed. Third element from first row, 5th from second row and length of second row are printed in step 2

---
+++ 02_Typy_danych/04_Tablice_2D/js/app.js
@@ -0,0 +1,17 @@
+const numbers = [
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12]
+];
+
+console.log(numbers[0][1]);
+console.log(numbers[1]);
+console.log(numbers[2].length);
+
+const mixedValues = [];
+mixedValues[0] = ["Keli", "Walter", "Heriberto"];
+mixedValues[1] = [1,2,3,4,5,6];
+
+console.log(mixedValues[0][2]);
+console.log(mixedValues[1][4]);
+console.log(mixedValues[1].length);
